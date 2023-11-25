e90f4d72d32f67c5d9ae209da4e7ed502a0819ae
Soda-art <142284380+Soda-art@users.noreply.github.com>
2023-11-25 19:24:49 +0700

    Update search-test.js

--- search-test.js
+++ search-test.js
@@ -1,41 +1,26 @@
-const inputSearch   = document.getElementById("job-search");
-const searchQuery   = inputSearch.value;
 
-const searchJob = async () => {
-
-    const inputSearch   = document.getElementById("job-search");
-    const searchQuery   = inputSearch.value;
-    console.log(searchQuery);
-
+const searchJob = async (page, limit, query) => {
     try {
-        const url   = `https://frcz3.sse.codesandbox.io/jobs?q=`+searchQuery;
+        const url   = `https://frcz3.sse.codesandbox.io/jobs?_page=${page}&_limit=${limit}&q=${query}`;
         const res   = await fetch(url);
         const data  = await res.json();
-        renderSearchJob(data);
+        renderJobs(data);
     } catch (err) {
         console.log('error', err);
     }
 }
 
-    inputSearch.addEventListener("keypress", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        searchJob();
-    }
-});
 
-const renderSearchJob = (data) => {
-    var html_render = '';
+async function renderJobs(data) {
+    let html = "";
     for (let i = 0; i < data.length; i++) {
-        html_render +=  `
-        div id="display-job">
-            <p class="title-job">Tilte-Job: ${data[i].title}</p>
-            <p class="city-job">City: ${data[i].city}</p>
-        </div>
-            `
-    };
-
-    console.log(html_render);
-    const display_job = document.getElementById("display-job");
-    display_job.innerHTML = html_render;
-}
+      html += `
+              <div id="display-job">
+              <p class="title-job">Tilte-Job: ${data[i].title}</p>
+              <p class="city-job">City: ${data[i].city}</p>
+          </div>
+              `;
+    }
+    const displayJob = document.getElementById("display-job");
+    displayJob.innerHTML = html;
+  }

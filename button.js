906b77c36df0a9ac8d48b04b1eef90aca05448e6
Soda-art <142284380+Soda-art@users.noreply.github.com>
2023-11-25 19:25:08 +0700

    Update button.js

--- button.js
+++ button.js
@@ -1,33 +1,38 @@
-const nextButton = document.querySelector('#next-button');
-const prevButton = document.querySelector('#prev-button');
-const displayJob = document.querySelector('#display-job');
+const apiUrl = "https://frcz3.sse.codesandbox.io/jobs";
+async function fetchData(page, limit) {
+  try {
+    const url = `${apiUrl}?_page=${page}&_limit=${limit}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error("Error fetching data:", error);
+  }
+}
 
-const nextPage = async () => {
-        try {
-        let limit = 10;
-        let page = 10;
-        let url = `https://frcz3.sse.codesandbox.io/jobs?_page=${page}&_limit=${limit}`;
-        const res = await fetch(url)
-        const data = await res.json();
-        getPage(data)
-        } catch (err) {
-            console.log('Err: ', err)
-    }
+async function renderJob(page, limit) {
+  const data = await fetchData(page, limit);
+  console.log(data);
+  let html = "";
+  for (let i = 0; i < data.length; i++) {
+    html += `
+            <div id="display-job">
+            <p class="title-job">Tilte-Job: ${data[i].title}</p>
+            <p class="city-job">City: ${data[i].city}</p>
+        </div>
+            `;
+  }
+  const displayJob = document.getElementById("display-job");
+  displayJob.innerHTML = html;
 }
-nextPage()
 
-let getPage = (data) => {
-    let html = '';
-    
-    for (let i = 0; i < data.length; i++) {
-        html += `
-        <div id="display-job">
-        <p class="title-job">Tilte-Job: ${data[i].title}</p>
-        <p class="city-job">City: ${data[i].city}</p>
-    </div>
-        `
+async function getNextPage() {
+  currentPage += 1;
+  renderJob(currentPage, limit);
 }
-console.log(html);
-const display_job = document.getElementById('display-job');
-display_job.innerHTML = html;
+
+async function getPrevPage() {
+  currentPage -= 1;
+  renderJob(currentPage, limit);
 }
+
